bc2a3a174b7ae04cfc50478bc637dc7535e8666d
Antoniue <Verstiplayingtm@gmail.com>
2022-10-03 13:08:20 +0300

    added routering and switched home page buttons to navlinks

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,5 @@
 import stars from '../Home/stars.png';
+import { NavLink } from 'react-router-dom'
 
 import '../Home/HomePage.css';
 function HomePage(){
@@ -14,11 +15,11 @@ function HomePage(){
                     <h1 className='nameLine'>Anton Jarayseh</h1>
                     <h2>I welcome you to sail through my Portfolio!</h2>
                 </div>
-                <div className="selections">
-                    <button className="aboutButton">About</button>
-                    <button className="projectsButton">Projects</button>
-                    <button className="contactButton">Contact</button>
-                </div>
+                <nav className="selections">
+                    <NavLink className="aboutButton">About</NavLink>
+                    <NavLink className="projectsButton">Projects</NavLink>
+                    <NavLink className="contactButton">Contact</NavLink>
+                </nav>
             </div>
         </div>
     )
