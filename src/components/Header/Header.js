146e6e6a499ffb3b2614a86d1750c7bec37a6268
Antoniue <Verstiplayingtm@gmail.com>
2022-10-04 09:14:37 +0300

    added header component, needs logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom';
+import '../Header/Header.css';
+function Header({navlist}){
+    return (
+        <div className="header">
+            <h1 className="headerLogo">Antoniue</h1>
+            <nav className="headerNavigation">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to={'/'+navlist[1].toLowerCase()}>{navlist[1]}</NavLink>
+                <NavLink to={'/'+navlist[2].toLowerCase()}>{navlist[2]}</NavLink>
+            </nav>
+        </div>
+    )
+}
+export {Header};
