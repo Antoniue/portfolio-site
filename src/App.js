cf54e3ecd16bb465e5510d9e8631e98f01ea56c1
Antoniue <Verstiplayingtm@gmail.com>
2022-10-04 16:54:50 +0300

    added route to projects page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { HomePage } from './components/Home/HomePage';
 import {Route, Routes } from "react-router-dom";
 import { AboutPage } from './components/About/AboutPage';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
       </Routes>
     </div>
   );
