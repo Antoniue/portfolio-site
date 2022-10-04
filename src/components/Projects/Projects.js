d00c1d64a5f29c46b683766770f9a97c03653310
Antoniue <Verstiplayingtm@gmail.com>
2022-10-04 18:00:59 +0300

    added projects component and a render projects component to render the projects array passed, WIP

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,23 @@
+import { Footer } from '../Footer/Footer';
+import { Header } from '../Header/Header';
+import '../Projects/Projects.css';
+import { RenderProjects } from './RenderProjects';
+import project0pic from '../Projects/project0pic.png';
+function Projects(){
+    const projects = [{
+        'summarized': {'name': 'Weather App', 'picture': {project0pic}, 'summary': 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+        'github': 'https://github.com/Antoniue/Weather-Watcher', 'demo': 'https://antoniue.github.io/Weather-Watcher/'},
+        'expanded': {'whyProject': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'difficulties': 'lorem ipsum lorem ipsum', 'howDealt': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'tools': 'test1 test2 test3 test4'}
+    }];
+    return(
+        <div className="content">
+            <Header navlist={['Home', 'About', 'Contact']}/>
+            <div className="projectsSpace">
+                <h1>Projects</h1>
+                <RenderProjects projects={projects}/>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+export {Projects};
---
+++ src/components/Projects/RenderProjects.js
@@ -0,0 +1,22 @@
+import githublogo from '../Footer/githublogo.png';
+import demologo from '../Projects/demologo.png';
+
+function RenderProjects({projects}){
+    console.log(projects);
+    return(
+        <div className="projectsContainer">
+            <div className="projectCard">
+                <h3 className="projectName">{projects[0].summarized.name}</h3>
+                <img src={projects[0].summarized.picture.project0pic} alt="" className="picture" />
+                <p className="summary">{projects[0].summarized.summary}</p>
+                <a href={projects[0].summarized.github} target='_blank' rel="noreferrer">
+                    <img src={githublogo} className='githubLogo' alt="github" />
+                </a>
+                <a href={projects[0].summarized.demo} target='_blank' rel="noreferrer">
+                    <img src={demologo} alt="demo project" className="demo"/>
+                </a>
+            </div>
+        </div>
+    )
+}
+export {RenderProjects};
