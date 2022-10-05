b79d24f581ee19c64c65085ff79d1b07b77e4fa7
Antoniue <Verstiplayingtm@gmail.com>
2022-10-05 14:29:52 +0300

    fixed some styles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ function Projects(){
         'github': 'https://github.com/Antoniue/Weather-Watcher', 'demo': 'https://antoniue.github.io/Weather-Watcher/'},
 
         'expanded': {'whyProject': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'difficulties': 'lorem ipsum lorem ipsum', 'howDealt': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'tools': 'test1 test2 test3 test4'}
-        },
+        }
     ];
     return(
         <div className="content">
