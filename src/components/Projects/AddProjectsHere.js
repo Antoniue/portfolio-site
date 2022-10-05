79750bd5176ebbbf01de5cca2cc3b265da2087bd
Antoniue <Verstiplayingtm@gmail.com>
2022-10-05 19:46:53 +0300

    added full project details to the temporary displaying weather app

--- src/components/Projects/AddProjectsHere.js
+++ src/components/Projects/AddProjectsHere.js
@@ -1,13 +1,22 @@
 import project0pic from '../Projects/project0pic.png';
+import jslogo from '../About/jslogo.svg';
+import htmllogo from '../About/htmllogo.ico';
+import csslogo from '../About/csslogo.webp';
+
 
 function AddProjectsHere(){
     const projects = 
     [
         {
-        'summarized': {'name': 'Weather App', 'picture': {project0pic}, 'summary': 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
-        'github': 'https://github.com/Antoniue/Weather-Watcher', 'demo': 'https://antoniue.github.io/Weather-Watcher/'},
+        'summarized': {'name': 'Weather App', 'picture': {project0pic},
+        'summary': 'A weather app where you can search a city and get weather data regarding that city',
+        'github': 'https://github.com/Antoniue/Weather-Watcher',
+        'demo': 'https://antoniue.github.io/Weather-Watcher/'},
 
-        'expanded': {'whyProject': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'difficulties': 'lorem ipsum lorem ipsum', 'howDealt': 'lorem ipsum lorem ipsum lorem ipsum lorem', 'tools': 'test1 test2 test3 test4'}
+        'expanded': {'whyProject': 'I wanted to practice fetching data from an API and using it',
+        'difficulties': "I didn't have major difficulties, only minor ones regarding the API itself",
+        'howDealt': 'I had to revisit some documentation on fetching data from the specific API (OpenWeatherMap)',
+        'tools': {jslogo, htmllogo, csslogo}}
         }
     ];
     return projects;
