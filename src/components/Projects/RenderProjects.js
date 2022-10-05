abf7fd2530732099b9c2674d39c4e7e089269c26
Antoniue <Verstiplayingtm@gmail.com>
2022-10-05 14:29:16 +0300

    added .map to render all projects in array given

--- src/components/Projects/RenderProjects.js
+++ src/components/Projects/RenderProjects.js
@@ -5,17 +5,25 @@ function RenderProjects({projects}){
     console.log(projects);
     return(
         <div className="projectsContainer">
-            <div className="projectCard">
-                <h3 className="projectName">{projects[0].summarized.name}</h3>
-                <img src={projects[0].summarized.picture.project0pic} alt="" className="picture" />
-                <p className="summary">{projects[0].summarized.summary}</p>
-                <a href={projects[0].summarized.github} target='_blank' rel="noreferrer">
-                    <img src={githublogo} className='github' alt="github" />
-                </a>
-                <a href={projects[0].summarized.demo} target='_blank' rel="noreferrer">
-                    <img src={demologo} alt="demo project" className="demo"/>
-                </a>
-            </div>
+            {
+                projects.map((project) => {
+                    return (
+                        <div className="projectCard">
+                            <h3 className="projectName">{project.summarized.name}</h3>
+                            <img src={project.summarized.picture.project0pic} alt="" className="picture" />
+                            <p className="summary">{project.summarized.summary}</p>
+                            <div>
+                                <a href={project.summarized.github} target='_blank' rel="noreferrer">
+                                    <img src={githublogo} className='github' alt="github" />
+                                </a>
+                                <a href={project.summarized.demo} target='_blank' rel="noreferrer">
+                                    <img src={demologo} alt="demo project" className="demo"/>
+                                </a>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
