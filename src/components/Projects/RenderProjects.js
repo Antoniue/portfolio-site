1d38494af3928524c00e34251e3a488017e482c6
Antoniue <Verstiplayingtm@gmail.com>
2022-10-05 14:48:22 +0300

    added a class for all buttons in project card for styling

--- src/components/Projects/RenderProjects.js
+++ src/components/Projects/RenderProjects.js
@@ -12,13 +12,14 @@ function RenderProjects({projects}){
                             <h3 className="projectName">{project.summarized.name}</h3>
                             <img src={project.summarized.picture.project0pic} alt="" className="picture" />
                             <p className="summary">{project.summarized.summary}</p>
-                            <div>
+                            <div className='projectButtons'>
                                 <a href={project.summarized.github} target='_blank' rel="noreferrer">
                                     <img src={githublogo} className='github' alt="github" />
                                 </a>
                                 <a href={project.summarized.demo} target='_blank' rel="noreferrer">
                                     <img src={demologo} alt="demo project" className="demo"/>
                                 </a>
+                                <button className='expandButton'>Expand &gt;</button>
                             </div>
                         </div>
                     )
