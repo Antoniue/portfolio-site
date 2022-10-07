bb90d5a40eaaa7e8f86fa1ced97671ab65bf205a
Antoniue <Verstiplayingtm@gmail.com>
2022-10-07 16:18:21 +0300

    changed default to light mode

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ReactSwitch from 'react-switch';
 export const themeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
     setTheme((current) => (current === 'light' ? 'dark':'light'));
