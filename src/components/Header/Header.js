000a9a948865dd95cd4a0c73c5fcd29e7a95e278
Antoniue <Verstiplayingtm@gmail.com>
2022-10-07 10:59:39 +0300

    removed burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,6 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../Header/Header.css';
 function Header({navlist}){
-    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
-    const [menu_class, setMenuClass] = useState("menu hidden")
-    const [isMenuClicked, setIsMenuClicked] = useState(false)
-
-    const updateMenu = () => {
-        if(!isMenuClicked) {
-            setBurgerClass("burger-bar clicked")
-            setMenuClass("menu visible")
-        }
-        else {
-            setBurgerClass("burger-bar unclicked")
-            setMenuClass("menu hidden")
-        }
-        setIsMenuClicked(!isMenuClicked)
-    }
 
     return (
         <header className="header">
@@ -26,12 +10,6 @@ function Header({navlist}){
                 <NavLink to={'/'+navlist[1].toLowerCase()}>{navlist[1]}</NavLink>
                 <NavLink to={'/'+navlist[2].toLowerCase()}>{navlist[2]}</NavLink>
             </nav>
-            <button className="burger-menu" onClick={updateMenu}>
-                <div className={burger_class}></div>
-                <div className={burger_class}></div>
-                <div className={burger_class}></div>
-            </button>
-            <div className={menu_class}></div>
         </header>
     )
 }
